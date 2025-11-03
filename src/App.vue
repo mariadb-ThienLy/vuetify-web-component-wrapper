f24c9cd812e57e9d1b60678dd65b7fd29205563c
mariadb-ThienLy <thien.ly@mariadb.com>
2025-11-03 12:07:36 +0200

    Custom sass variable example

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-tooltip text="Tooltip">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props">Hover Over Me</v-btn>
+        </template>
+      </v-tooltip>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
 
-  //
+//
 </script>
