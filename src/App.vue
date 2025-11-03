71c05564a28509d4526e8e1f65c87d5ba1f99ab1
mariadb-ThienLy <thien.ly@mariadb.com>
2025-11-03 12:11:39 +0200

    Add tooltip content text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-tooltip text="Tooltip">
+      <v-tooltip text="Tooltip text should be white, background color should be red">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props">Hover Over Me</v-btn>
         </template>
